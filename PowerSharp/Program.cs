3cf0d0e1bb243a2b969fce921a6834f7f7bbd294
LostZerglings <jsutowski@gmail.com>
2018-09-15 17:55:26 -0400

    test commit

--- PowerSharp/Program.cs
+++ PowerSharp/Program.cs
@@ -5,18 +5,20 @@
     class Program
     {
         /// <summary>
+        /// 
         /// The following code shows a simple example of how to run a powershell script and dispaly the reponse in a console window. 
         /// 
         /// The Main method displays a menu of available scripts. 
         /// 
         /// The individual scripts are located in the Scripts Folder.
+        /// 
         /// </summary>
 
         static void Main(string[] args)
         {
             //Display initial menu
             string menu = "Please choose an option:\n(1) ping a website";
-            string lineBreak = "\n=============================================\n";
+            string lineBreak = "\n============================================\n";
             Console.WriteLine(menu);
 
             var command = string.Empty;
