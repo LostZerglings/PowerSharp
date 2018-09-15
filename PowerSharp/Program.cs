b08d6be83b8f3ee4d8a0eb787ec0380023bc44c5
Joshua Sutowski <jsutowski@gmail.com>
2018-09-15 17:49:20 -0400

    Added ping script

--- PowerSharp/Program.cs
+++ PowerSharp/Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PowerSharp
+﻿namespace PowerSharp
 {
+    using System;
+
     class Program
     {
+        /// <summary>
+        /// The following code shows a simple example of how to run a powershell script and dispaly the reponse in a console window. 
+        /// 
+        /// The Main method displays a menu of available scripts. 
+        /// 
+        /// The individual scripts are located in the Scripts Folder.
+        /// </summary>
+
         static void Main(string[] args)
         {
+            //Display initial menu
+            string menu = "Please choose an option:\n(1) ping a website";
+            string lineBreak = "\n=============================================\n";
+            Console.WriteLine(menu);
+
+            var command = string.Empty;
+            while(command != "exit")
+            {
+                command = Console.ReadLine();
+                Console.WriteLine(lineBreak);
+
+                //Run the matching script
+                if (command == "1")
+                    Console.WriteLine(Scripts.Ping.PingResult());
+
+                Console.WriteLine(lineBreak);
+                Console.WriteLine(menu);
+            }
         }
     }
 }
---
+++ PowerSharp/Scripts/Ping.cs
@@ -0,0 +1,45 @@
+﻿namespace PowerSharp.Scripts
+{
+    using System;
+    using System.Management.Automation;
+
+    class Ping
+    {
+        /// <summary>
+        /// 
+        /// A simple script that will ping a given website and return the results as a string. 
+        /// If no url is given the script will ping google.com
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+
+        public static string PingResult(string url = "google.com")
+        {
+            try
+            {
+                string pingResult = string.Empty;
+
+                //load a powershell instance
+                using (PowerShell powerShell = PowerShell.Create())
+                {
+                    //add the script to the instance
+                    powerShell.AddScript($@"ping {url}");
+
+                    //iterate through the results
+                    foreach (PSObject result in powerShell.Invoke())
+                    {
+                        pingResult += result.BaseObject.ToString() + Environment.NewLine;
+                    }
+                }
+
+                //return the results
+                return pingResult;
+            }
+            catch (Exception error)
+            {
+                return error.ToString();
+            }
+        }
+    }
+}
