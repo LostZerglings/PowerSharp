9f626cc643666fdb43fa25fd661d41f7c050e97a
Joshua Sutowski <jsutowski@gmail.com>
2018-09-15 17:19:52 -0400

    first commit

---
+++ PowerSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
